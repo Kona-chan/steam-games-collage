6c0d780d37a6e8f8c08aa1aa04120b98980ea258
Konata Izumi <kona-chan@eureka7.ru>
2012-07-21 00:58:22 +0300

    Added cleanup routine and basic syntax check

--- steam_icons.sh
+++ steam_icons.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+function cleanup() {
+    if [ -e "urls.temp" ] ; then rm urls.temp ; fi
+}
+trap cleanup INT EXIT QUIT TERM
+
+if [ -z "$1" ] 
+then
+    echo "Syntax: ./steam_icons.sh steam_username"
+    exit
+fi
+
 username="$1"
 profile_url="http://steamcommunity.com/id/$username/games?tab=all&xml=1"
 
 wget -nv -O- $profile_url |\
     grep appID |\
     egrep -o [0-9]+ |\
-    sed -e 's#\(.*\)#http://cdn.steampowered.com/v/gfx/apps/\1/header_292x136.jpg#' |\
+    sed -e 's#\(.*\)#http://cdn.steampowered.com/v/gfx/apps/\1/header_292x136.jpg#' \
+    > urls.temp
