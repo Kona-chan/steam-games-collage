1051586a4a197708cbbb5de749273da8f0340b44
Konata Izumi <kona-chan@eureka7.ru>
2012-07-21 16:22:06 +0300

    Added rounded corners. Script is now fully functional, but games don't appear in alphabetical order.

--- steam_icons.sh
+++ steam_icons.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 function cleanup() {
-    if [ -e "urls.temp" ] ; then rm urls.temp ; fi
+    if [ -d "steam_icons" ] ; then rm -r steam_icons ; fi
 }
 trap cleanup INT EXIT QUIT TERM
 
-if [ -z "$1" ] 
-then
+if [ -z "$1" ] ; then
     echo "Syntax: ./steam_icons.sh steam_username"
     exit
 fi
@@ -14,8 +13,44 @@ fi
 username="$1"
 profile_url="http://steamcommunity.com/id/$username/games?tab=all&xml=1"
 
-wget -nv -O- $profile_url |\
+echo "Fetching icons..."
+
+if [ -d "steam_icons" ] ; then
+    rm -r steam_icons
+fi
+mkdir steam_icons
+
+wget -q -O- $profile_url |\
     grep appID |\
     egrep -o [0-9]+ |\
-    sed -e 's#\(.*\)#http://cdn.steampowered.com/v/gfx/apps/\1/header_292x136.jpg#' \
-    > urls.temp
+    sed -e 's#\(.*\)#http://cdn.steampowered.com/v/gfx/apps/\1/header_292x136.jpg#' |\
+    wget -q -i- -P ./steam_icons
+
+function round_corner() {
+    convert $1 \
+    \( +clone  -alpha extract \
+        -draw 'fill black polygon 0,0 0,15 15,0 fill white circle 15,15 15,0' \
+        \( +clone -flip \) -compose Multiply -composite \
+        \( +clone -flop \) -compose Multiply -composite \
+    \) -alpha off -compose CopyOpacity -composite $1.png
+}
+
+echo "Applying rounded corners..."
+
+for i in $( find steam_icons -iname '*jpg*' ) ; do
+    round_corner "$i"
+done
+
+function make_grid() {
+    montage \
+        -background "#292929" \
+        -tile 8x \
+        -geometry +1+1 \
+        steam_icons/*.png $1
+}
+
+echo "Merging grid..."
+
+make_grid "montage.jpg"
+
+echo "Done!"
